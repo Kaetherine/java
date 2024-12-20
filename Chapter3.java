da65b7c48e087a74e2ea26cbfe5ca72d6997c915
Kaetherine <kuhle.katherine@googlemail.com>
2024-12-20 20:58:03 +0100

    /

--- Chapter3.java
+++ Chapter3.java
@@ -1,5 +1,8 @@
 public class Chapter3 {
     public static void main(String[] args){
-        System.out.println("Chapter3");
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+        int num;
+        num = scan.nextInt();
+        System.out.println(num);
     }
 }
